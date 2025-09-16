1687e00881424c63c3169d3e54df5d0f0148237f
Prince Bhatt <sharmaprince89667@gmail.com>
2025-09-16 11:57:40 +0530

    Project Started, Repo Setup Done

---
+++ Client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
+import About from './pages/AboutPage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* Routes */}
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
